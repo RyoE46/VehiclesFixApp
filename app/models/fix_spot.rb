9919f648ae9bad6a5870358d73012dde5435f994
RyoE46 <mido88ryo@gmail.com>
2022-12-28 17:45:05 +0900

    ビュー調整

--- app/models/fix_spot.rb
+++ app/models/fix_spot.rb
@@ -1,16 +1,15 @@
 class FixSpot < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: 'エンジン' },
-    { id: 3, name: '吸気系・排気系' },
-    { id: 4, name: 'エンジン電装' },
-    { id: 5, name: 'ミッション・駆動系' },
-    { id: 6, name: '足回り' },
-    { id: 7, name: 'ブレーキ系統' },
-    { id: 8, name: 'ボディ関連' },
-    { id: 9, name: '一般電装・その他電装系' },
-    { id: 10, name: '内装・アクセサリー' },
-    { id: 11, name: 'その他' },
+    { id: 1, name: 'エンジン' },
+    { id: 2, name: '吸気系・排気系' },
+    { id: 3, name: 'エンジン電装' },
+    { id: 4, name: 'ミッション・駆動系' },
+    { id: 5, name: '足回り' },
+    { id: 6, name: 'ブレーキ系統' },
+    { id: 7, name: 'ボディ関連' },
+    { id: 8, name: '一般電装・その他電装系' },
+    { id: 9, name: '内装・アクセサリー' },
+    { id: 10, name: 'その他' },
 
   ]
 
